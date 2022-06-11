623f8659839f676a5bec3a7fcafe10304a95bb77
hchengwang <hchengwang@gmail.com>
2022-06-11 20:54:57 +0800

    add funciton overlading for pybind11

--- examples/cpplabs/pybind11/SegList.cpp
+++ examples/cpplabs/pybind11/SegList.cpp
@@ -9,6 +9,12 @@ void init_SegList(py::module &m) {
     
     py::class_<SegList>(m, "SegList")
     .def(py::init())
+    .def("addVertex", 
+            static_cast<void (SegList::*)(double, double)>(&SegList::addVertex),
+            "add vertex with x, y")
+    .def("addVertex", 
+            static_cast<void (SegList::*)(Vertex)>(&SegList::addVertex),
+            "add vertex with a vertex")
     //.def("setRandom", &SegList::setRandom)
     .def("getSpec", &SegList::getSpec);
 }
--- examples/cpplabs/pybind11/Vertex.cpp
+++ examples/cpplabs/pybind11/Vertex.cpp
@@ -9,6 +9,15 @@ void init_Vertex(py::module &m) {
     
     py::class_<Vertex>(m, "Vertex")
     .def(py::init())
-    //.def("setRandom", &Vertex::setRandom)
+    .def(py::init<int, int>())
+    .def("setXY", &Vertex::setXY)
+    .def("getX", &Vertex::getX)
+    .def("getY", &Vertex::getY)
+    .def("setRandom", 
+            static_cast<void (Vertex::*)(int, int)>(&Vertex::setRandom), 
+            "set max/min")
+    .def("setRandom", 
+            static_cast<void (Vertex::*)(int, int, int, int)>(&Vertex::setRandom), 
+            "set x, y max/min")
     .def("getSpec", &Vertex::getSpec);
 }
